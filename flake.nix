b3abf465fb4e8f6ee72a8b6da1b2c53b68581af3
aristote <quentin.aristote@irif.fr>
2025-08-01 12:46:16 +0200

    flake: data: switch branch
    • Updated input 'data':
        'github:qaristote/info/9749429636e7e5e2515f1c75d228d5321907fba4' (2025-07-27)
      → 'github:qaristote/info/7132af9e3a7f84cebe287b3174f2d4a9c25252ca' (2025-08-01)

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Source code of Quentin Aristote's personal webpage.";
 
   inputs = {
-    data.url = "github:qaristote/info/new-pub-format";
+    data.url = "github:qaristote/info/master";
     my-nixpkgs.url = "github:qaristote/my-nixpkgs";
     uncss = {
       url = "github:qaristote/uncss";
@@ -10,40 +10,47 @@
         nixpkgs.follows = "/nixpkgs";
       };
     };
-    nixpkgs = {};
+    nixpkgs = { };
   };
 
-  outputs = {
-    flake-parts,
-    my-nixpkgs,
-    ...
-  } @ inputs:
-    flake-parts.lib.mkFlake {inherit inputs;} ({lib, ...}: {
-      imports = builtins.attrValues {inherit (my-nixpkgs.flakeModules) personal;};
+  outputs =
+    {
+      flake-parts,
+      my-nixpkgs,
+      ...
+    }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } (
+      { lib, ... }:
+      {
+        imports = builtins.attrValues { inherit (my-nixpkgs.flakeModules) personal; };
 
-      flake.lib = import ./lib {inherit lib;};
+        flake.lib = import ./lib { inherit lib; };
 
-      perSystem = {
-        self',
-        pkgs,
-        system,
-        ...
-      }: let
-        pkgs' = pkgs.extend (
-          _: _: {
-            uncss = inputs.uncss.packages."${system}".default;
-            line-awesome-css = my-nixpkgs.packages."${system}".static_css_lineAwesome;
-          }
-        );
-      in {
-        packages = {
-          default = self'.packages.webpage;
-          webpage = pkgs'.callPackage ./default.nix {
-            nixpkgsSrc = inputs.nixpkgs.outPath;
-            src = pkgs'.callPackage ./src.nix {};
-            data = inputs.data.packages."${system}".src;
+        perSystem =
+          {
+            self',
+            pkgs,
+            system,
+            ...
+          }:
+          let
+            pkgs' = pkgs.extend (
+              _: _: {
+                uncss = inputs.uncss.packages."${system}".default;
+                line-awesome-css = my-nixpkgs.packages."${system}".static_css_lineAwesome;
+              }
+            );
+          in
+          {
+            packages = {
+              default = self'.packages.webpage;
+              webpage = pkgs'.callPackage ./default.nix {
+                nixpkgsSrc = inputs.nixpkgs.outPath;
+                src = pkgs'.callPackage ./src.nix { };
+                data = inputs.data.packages."${system}".src;
+              };
+            };
           };
-        };
-      };
-    });
+      }
+    );
 }
